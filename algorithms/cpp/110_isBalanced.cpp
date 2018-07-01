ac4aab5d470472148271692fc9b092239fc34d37
Tramac <1145955920@qq.com>
2018-07-01 02:28:58 -0500

    Update 110_isBalanced.cpp

--- algorithms/cpp/110_isBalanced.cpp
+++ algorithms/cpp/110_isBalanced.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 struct TreeNode
 {
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 int depth(TreeNode* root)
 {
-	if(root == NULL)
-		return 0;
-	return max(depth(root->left) + 1, depth(root->right) + 1);
+    if(root == NULL)
+	return 0;
+    return max(depth(root->left) + 1, depth(root->right) + 1);
 }
 
 bool isBalanced(TreeNode* root)
 {
-	if(root == NULL)
-		return true;
-	int left = depth(root->left);
-	int right = depth(root->right);
-	if(left - right > 1 || right - left > 1)
-		return false;
-	return isBalanced(root->left) && isBalanced(root->right);
+    if(root == NULL)
+	return true;
+    int left = depth(root->left);
+    int right = depth(root->right);
+    if(left - right > 1 || right - left > 1)
+	return false;
+    return isBalanced(root->left) && isBalanced(root->right);
 }
