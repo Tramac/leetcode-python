7382bce40603173c2a50bb69f2a0b6cc6f375c10
Tramac <1145955920@qq.com>
2018-07-02 09:39:58 -0500

    Create 232_MyQueue.cpp

---
+++ algorithms/cpp/232_MyQueue.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    /** Initialize your data structure here. */
+    MyQueue()
+    {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+		s1.push(x);
+	}
+
+	/** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if(s2.empty())
+		{
+			while(!s1.empty())
+			{
+				s2.push(s1.top());
+				s1.pop();
+			}
+		}
+		int temp = s2.top();
+		s2.pop();
+		return temp;
+	}
+
+	/** Get the front element. */
+    int peek() {
+        if(s2.empty())
+		{
+			while(!s1.empty())
+			{
+				s2.push(s1.top());
+				s1.pop();
+			}
+		}
+		int temp = s2.top();
+		return temp;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+		return s1.empty() && s2.empty();
+	}
+};
