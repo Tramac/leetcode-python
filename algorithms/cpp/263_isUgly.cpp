48c01443558092678fb2ad49c71951b27f9dcb59
Tramac <1145955920@qq.com>
2018-07-02 22:41:43 -0500

    Create 263_isUgly.cpp

---
+++ algorithms/cpp/263_isUgly.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+bool isUgly(int num)
+{
+	if(num == 0)
+		return false;
+	while (num % 5 == 0)
+		num /= 5;
+	while(num % 3 == 0)
+		num /= 3;
+	while(num % 2 == 0)
+		num /= 2;
+	if(num == 1 || num == 2 || num == 3 || num == 5)
+		return true;
+	return false;
+}
