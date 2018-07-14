cf6d4455f70122956196c6b95f34695b3979abfd
Tramac <1145955920@qq.com>
2018-07-14 07:38:34 -0500

    Create 338_countBits.cpp

---
+++ algorithms/cpp/338_countBits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> countBits(int num)
+{
+	vector<int> res(num + 1, 0);
+	for (int i = 1; i <= num; ++i)
+	{
+		res[i] = res[i & (i - 1)] + 1;
+	}
+	return res;
+}
